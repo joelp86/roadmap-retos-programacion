9484490f7c7035df4849428b717eec33fb14da7a
joelp86 <chulepereyra14@gmail.com>
2024-05-18 11:21:46 -0300

    Este es un commit

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/joelp86.js
@@ -0,0 +1,41 @@
+//Documentacion oficial de Javascript:  https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// "Este es un comentario en una linea"
+
+/* "Este es un comentario 
+en varias 
+lineas" */
+
+// "Variable"
+var nombre = "Joel";
+
+//"Constante"
+const PI = 3.14;
+
+//Variable tipo de datos primitivos:
+
+//CADENA DE TEXTO:string
+var string = "Hola a todos";
+
+// Numeros
+
+var numeroEntero = 17;
+var numeroNegativo = -8;
+var numeroDecimal = 11.45;
+
+//BOOLEANOS
+var estoyEntendiendo = true;
+var estoyAburrido= false;
+
+//UNDEFINED
+
+var caja1;
+
+//NULL
+
+var sinNada = null;
+
+// USANDO LA TERMINAL:
+
+var lenguaje = "Javascript";
+console.log("hola,Javascript");
